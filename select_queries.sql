cdaad9ebf42b803ba4c7682c7755fee0ebe4da6a
Victor Johnson <johnsonsirv@gmail.com>
2019-06-23 23:50:01 +0200

    using ROUND function to round value of expressions in SELECT

--- select_queries.sql
+++ select_queries.sql
@@ -15,3 +15,23 @@ WHERE population >= 200000000
 
 SELECT name, gdp/population As 'per capita GDP' from world
 WHERE population >= 200000000
+
+SELECT name, population/1000000 As 'populaion in millions' from world
+WHERE continent = 'South America'
+
+SELECT name, population from world
+WHERE name = 'France' or name ='Germany' or name = 'Italy'
+
+SELECT name from world
+WHERE name like '%United%'
+
+SELECT name, population, area from world
+WHERE area >3000000 or population >250000000
+
+-- Exclusive OR (XOR). Show the countries that are big by area or big by population but not both. Show name, population and area.
+SELECT name, population, area from world
+WHERE area >3000000 xor population >250000000
+
+-- For South America continent show population in millions and GDP in billions both to 2 decimal places
+SELECT name, ROUND(population/1000000, 2) As 'population in millions', ROUND(gdp/1000000000,2) As 'GDP in billions' from world
+WHERE continent = 'South America'
