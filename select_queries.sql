f55978cb171ad6986a134f92e1c86182858844cd
Victor Johnson <johnsonsirv@gmail.com>
2019-06-24 01:08:11 +0200

    NOT LIKE '% %' to exlcude names that contain space (more than one word)

--- select_queries.sql
+++ select_queries.sql
@@ -48,3 +48,13 @@ SELECT name, capital  FROM world
 SELECT name,  capital
 FROM world
 WHERE LEFT(name,1) = LEFT(capital,1) AND capital <> name
+
+-- Find the country that has all the vowels and no spaces in its name.
+SELECT name
+   FROM world
+WHERE name LIKE '%a%'
+AND name LIKE '%e%'
+AND name LIKE '%i%'
+AND name LIKE '%o%'
+AND name LIKE '%u%'
+AND name NOT LIKE '% %'
