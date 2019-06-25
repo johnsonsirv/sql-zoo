d27da764fb056159f563a953fc4d6d2eb0da731e
Victor Johnson <johnsonsirv@gmail.com>
2019-06-25 12:35:19 +0200

    use case-then-else in order by clause

--- select_queries.sql
+++ select_queries.sql
@@ -58,3 +58,13 @@ AND name LIKE '%i%'
 AND name LIKE '%o%'
 AND name LIKE '%u%'
 AND name NOT LIKE '% %'
+
+-- The expression subject IN ('Chemistry','Physics') can be used as a value - it will be 0 or 1.
+
+-- Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
+SELECT winner, subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY 
+  CASE WHEN subject IN ('Physics','Chemistry') THEN 1 ELSE 0 END,
+   subject, winner
