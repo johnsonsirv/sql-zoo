e299e428bb658cb17c8ee130fbe6d25fd7ee016c
Victor Johnson <johnsonsirv@gmail.com>
2019-06-26 19:48:38 +0200

    execution order

--- select_queries.sql
+++ select_queries.sql
@@ -93,3 +93,14 @@ WHERE building IS NULL;
 
 SELECT role, avg(years_employed) FROM employees
 group by role;
+
+SELECT role, SUM(years_employed)
+FROM employees
+GROUP BY role
+HAVING role = "Engineer";
+
+SELECT director, SUM(domestic_sales + international_sales) as Cumulative_sales_from_all_movies
+FROM movies
+    INNER JOIN boxoffice
+        ON movies.id = boxoffice.movie_id
+GROUP BY director;
